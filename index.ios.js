f7da7b82fde05261af94566c551a520559483c9d
devmeyster <arthurfbi@yahoo.com>
2016-05-07 17:13:56 -0700

    set up react native app

---
+++ index.ios.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+
+import Main from './App/Components/Main';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  NavigatorIOS,
+  View
+} from 'react-native';
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111111'
+  }
+});
+
+class githubNotetaker extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Github Notetaker',
+          component: Main
+        }}/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
